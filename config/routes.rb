9089afdbf87c404fc8f5b4da9977fb910757c2ab
正典 <masanori@sakuranoMacBook-Air.local>
2023-11-23 19:16:17 +0900

    エラーメッセージ表示

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "items#index"
+  resources :items do
+    resources :purchases
+  end
 end
