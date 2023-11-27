669215ce78c1243b96d033c32cd12c3607a1f136
正典 <masanori@sakuranoMacBook-Air.local>
2023-11-27 21:09:23 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :last_name_full
     validates :first_name_full
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角文字を使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
