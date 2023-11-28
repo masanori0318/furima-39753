cf6d2febf33dbea9fc19b907a2db17581e3fb616
正典 <masanori@sakuranoMacBook-Air.local>
2023-11-28 18:18:48 +0900

    モデル、テーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+class Item < ApplicationRecord
+  validates :product_name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :conditiion_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  belongs_to :user
+  has_one_attached :image
+  #has_one :purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :conditiion
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+end
