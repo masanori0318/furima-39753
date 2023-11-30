e0f4801b12e017a6331fbbc57f5c531c073a39ab
正典 <masanori@sakuranoMacBook-Air.local>
2023-11-30 20:37:17 +0900

    販売手数料や利益計算結果を表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,13 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById('item-price');
+  const addTax = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTax.innerHTML = (Math.floor(inputValue/10));
+    profit.innerHTML = inputValue - (Math.floor(inputValue/10));
+  })
+
+})
