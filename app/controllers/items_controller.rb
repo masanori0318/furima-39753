42992b6ccd81c2778b5be032625fda952861e842
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2023-12-07 17:40:04 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
+    if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
     end
     redirect_to action: :index
