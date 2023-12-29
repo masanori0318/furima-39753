0441af6c50745bc2837cb0b552b6366d8cb92e68
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2023-12-29 22:04:52 +0900

    デプロイのための修正

--- db/migrate/20231226123851_add_purchase_id_to_addresses.rb
+++
@@ -1,5 +0,0 @@
-class AddPurchaseIdToAddresses < ActiveRecord::Migration[7.0]
-  def change
-    add_column :addresses, :purchase_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_26_123851) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_113617) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,9 +46,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_123851) do
     t.string "house_number", null: false
     t.string "building"
     t.string "phone_number", null: false
+    t.bigint "purchase_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "purchase_id"
+    t.index ["purchase_id"], name: "index_addresses_on_purchase_id"
   end
 
   create_table "categories", charset: "utf8", force: :cascade do |t|
@@ -120,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_123851) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "purchases"
   add_foreign_key "items", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
