bf467f7f54577f6f860fac25e2c965266b7f6487
masanori0318 <144527559+masanori0318@users.noreply.github.com>
2024-01-06 17:57:54 +0900

    JavaScript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,4 @@
-window.addEventListener("turbo:load", function() {
-
+const price = () => {
   const priceInput = document.getElementById('item-price');
   const addTax = document.getElementById('add-tax-price');
   const profit = document.getElementById('profit');
@@ -10,4 +9,6 @@ window.addEventListener("turbo:load", function() {
     addTax.innerHTML = (Math.floor(inputValue/10));
     profit.innerHTML = inputValue - (Math.floor(inputValue/10));
   })
-})
+}
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
